cd6fae859f186df267efda8d752ecaf89343494e
Duchuy <sanhana0609@gmail.com>
2026-03-09 11:58:54 +0100

    color

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+from utlis.dataprocessing import data
+st.title("Testrunapp")
+st.write("Duchuy")
+  #  "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+st.divider()
+st.subheader("Dữ liệu tội phạm (Raw Data)")
+st.dataframe(data)
+
+my_numbers = [1,2,3]
+st.divider()
+st.line_chart(my_numbers)
+ 
